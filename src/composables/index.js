d2a9e2889443cc71b4d86eeb48d216fb7226e6ce
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-10-22 19:07:52 +0300

    live currency list, live currency pair data and , time series apis has been developed

---
+++ src/composables/index.js
@@ -0,0 +1,98 @@
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+async function fetchLiveCurrencyList () {
+    const currencies = {
+        data: [],
+        status: false,
+        errorMessage: ""
+    }
+
+    try {
+        const url = `${import.meta.env.VITE_REST_API_URL}/api/v1/live_currencies_list?&api_key=${import.meta.env.VITE_REST_API_KEY}`;
+
+        const response = await fetch(url, requestOptions);
+        const responseJson = await response.json();
+
+        if (responseJson.available_currencies) {
+            currencies.status = true;
+            currencies.data = Object.keys(responseJson.available_currencies).map(currency => {
+                return {
+                    id: currency,
+                    title: responseJson.available_currencies[currency]
+                }
+            });
+        }else{
+            throw new Error(responseJson.message);
+        }
+
+    } catch (error) {
+        currencies.errorMessage = error.message;
+    }
+
+    return currencies;
+}
+
+
+async function fetchLiveCurrencyPair (selectedCurrencyPair) {
+    const currencyPairResponse = {
+        data: [],
+        status: false,
+        errorMessage: ""
+    }
+
+    try {
+        const url = `${import.meta.env.VITE_REST_API_URL}/api/v1/live?currency=${selectedCurrencyPair}&api_key=${import.meta.env.VITE_REST_API_KEY}`;
+        const response = await fetch(url, requestOptions);
+        const responseJson = await response.json();
+
+        if (responseJson.quotes) {
+            currencyPairResponse.status = true;
+            currencyPairResponse.data = responseJson.quotes[0]
+        } else {
+            throw new Error(responseJson.message);
+        }
+
+    } catch (error) {
+        currencyPairResponse.errorMessage = error.message;
+    }
+
+    return currencyPairResponse;
+}
+
+
+async function fetchTimeSeries (params) {
+    const timeSeries = {
+        data: [],
+        status: false,
+        errorMessage: ""
+    }
+
+    try {
+        const url = new URL(`${import.meta.env.VITE_REST_API_URL}/api/v1/timeseries?&api_key=${import.meta.env.VITE_REST_API_KEY}`);
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+        const response = await fetch(url.href, requestOptions);
+        const responseJson = await response.json();
+
+        if (responseJson.quotes) {
+            timeSeries.status = true;
+            timeSeries.data = responseJson;
+        }
+        else{
+            throw new Error(responseJson.message);
+        }
+
+    } catch (error) {
+        timeSeries.errorMessage = error.message;
+    }
+
+    return timeSeries;
+}
+
+export {
+    fetchLiveCurrencyList,
+    fetchLiveCurrencyPair,
+    fetchTimeSeries,
+}
