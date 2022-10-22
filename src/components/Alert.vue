c6b058374ff71696b04fa50d11e46209d667a365
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-10-22 18:44:37 +0300

    Alert component has been developed

---
+++ src/components/Alert.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  alert: {
+    type: Object,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <div :class="['alert',`alert-${props.alert.type}`]">
+    {{ props.alert.message }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/scss/local/_index.scss";
+
+.alert {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 8px;
+  color: $white;
+  border-radius: 9999px;
+  font-size: 13px;
+  @include breakpoint("lg") {
+    font-size: 14px;
+  }
+  @include breakpoint("2xl") {
+    font-size: 15px;
+    padding: 10px;
+  }
+}
+
+@each $key, $value in $colors {
+  .alert-#{$key} {
+    background-color: rgba($value, .95);
+  }
+}
+</style>
