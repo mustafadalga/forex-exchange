32b3f8be48981c00577879fc38ef675af776bea8
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-10-22 18:43:31 +0300

    Socket class has been developed

---
+++ src/classes/Socket.js
@@ -0,0 +1,42 @@
+import * as io from 'socket.io-client';
+
+class Socket {
+    constructor () {
+        const options = { reconnect: true };
+        const url = import.meta.env.VITE_STREAMING_API_URL;
+        this.socket = io.connect(url, options);
+    }
+
+    connect () {
+        this.socket.on('connect', () => {
+            this.socket.emit('login', { userKey: import.meta.env.VITE_STREAMING_API_KEY });
+        });
+    }
+
+    emitCurrencyPair (currencyPair) {
+        this.socket.emit("symbolSub", { symbol: currencyPair });
+    }
+
+    handleLiveCurrencyPair (currencyPair, liveCurrencyPairData) {
+        this.socket.on('price', (response) => {
+            const data = response.split(" ");
+
+            if (data[0] == currencyPair.value) {
+                liveCurrencyPairData.value = {
+                    pair: data[0],
+                    bid: data[1],
+                    ask: data[2],
+                    mid: data[3],
+                    date: data[4]
+                }
+            }
+        });
+    }
+
+    disconnect () {
+        this.socket.on('disconnect', (message) => {
+        });
+    }
+}
+
+export default Socket;
