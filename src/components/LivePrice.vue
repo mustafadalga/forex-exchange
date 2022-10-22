27b5f9162f2054f2ec04f28621145ede9e50f983
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-10-22 19:03:05 +0300

    live price component has been developed

---
+++ src/components/LivePrice.vue
@@ -0,0 +1,34 @@
+<script setup>
+import getSymbolFromCurrency from 'currency-symbol-map'
+
+const props = defineProps({
+  unit: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="live-price">
+    {{ getSymbolFromCurrency(props.unit) }} {{ props.price.bid }}
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+@import "@/scss/local/_index.scss";
+.live-price {
+  text-align: end;
+  font-weight: bold;
+  font-size: 18px;
+  color: $black;
+  @include breakpoint("lg") {
+    font-size: 25px;
+  }
+}
+</style>
