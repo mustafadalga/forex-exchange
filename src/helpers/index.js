ae934c1e4a7a2bb772d2a7e46fc2ff993878325a
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-10-22 18:45:51 +0300

    getTimestampByDate,getFridayDateOfWeekByDate,isWeekend helper functions has been developed

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,31 +1,69 @@
 import moment from "moment";
 
-function getPriceDiff (type, quotes) {
-    const now = moment(moment.now()).format("YYYY-MM-DD");
-    const lastDay = quotes.find(quote => quote.date == now);
-    const previousDay = quotes.find(quote => quote.date == getPreviousDate(type));
+function getPriceDiff (type, amount, quotes) {
+    const ago = new Date(getPreviousDate(type, amount));
+    const startDay = isWeekend(ago) ? getFridayDateOfWeekByDate(ago) : getPreviousDate(type, amount);
+    const endDay = isWeekend() ? getFridayDateOfWeekByDate() : getTodayDate();
+    const startDate = quotes.find(quote => quote.date == startDay);
+    const endDate = quotes.find(quote => quote.date == endDay);
+
+    if (startDate) {
+        return {
+            diff: endDate.close - startDate.open,
+            rate: getRateOfChange(startDate.open, endDate.close)
+        };
+    }
 
     return {
-        diff: lastDay.close - previousDay.open,
-        rate: getRateOfChange(previousDay.open, lastDay.close)
-    };
+        diff: "No Data",
+        rate: "No Data",
+    }
 }
+
 
 function getRateOfChange (open, close) {
     return (close - open) / open * 100
 }
 
-function getPreviousDate (type) {
-    return moment(moment.now()).subtract(1, type).format("YYYY-MM-DD")
+function getPreviousDate (type, amount = 1) {
+    return moment(moment.now()).subtract(amount, type).format("YYYY-MM-DD")
 }
 
 function getTodayDate () {
     return moment(moment.now()).format("YYYY-MM-DD");
 }
 
+function getTimestampByDate (date) {
+    return moment(date, 'YYYY-MM-DD').format('x');
+}
+
+function isWeekend (date = new Date()) {
+    return date.getDay() === 6 || date.getDay() === 0;
+}
+
+
+function getFridayDateOfWeekByDate (date = new Date()) {
+    const fridayDate = date;
+
+    if (fridayDate.getDay() == 6) {
+        fridayDate.setDate(fridayDate.getDate() - 1);
+    } else if (fridayDate.getDay() == 0) {
+        fridayDate.setDate(fridayDate.getDate() - 2);
+    } else {
+        const weekDaysCount = 5;
+        const friday = weekDaysCount - fridayDate.getDay();
+        fridayDate.setDate(fridayDate.getDate() + friday);
+    }
+
+    return moment(fridayDate).format("YYYY-MM-DD");
+}
+
 export {
     getPriceDiff,
     getRateOfChange,
     getPreviousDate,
-    getTodayDate
+    getTimestampByDate,
+    getTodayDate,
+    getFridayDateOfWeekByDate,
+    isWeekend
 }
