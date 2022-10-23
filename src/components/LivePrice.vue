3cbcf64dfe37a4cfd3dc670118e837b183ccae70
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-10-23 23:57:37 +0300

    live price changes animation has been developed

--- src/components/LivePrice.vue
+++ src/components/LivePrice.vue
@@ -1,5 +1,6 @@
 <script setup>
 import getSymbolFromCurrency from 'currency-symbol-map'
+import { ref, watch } from "vue";
 
 const props = defineProps({
   unit: {
@@ -10,18 +11,38 @@ const props = defineProps({
     type: Object,
     required: true
   }
-})
+});
+
+const showAnimation = ref(false);
+let timeoutID;
+
+
+watch(() => props.price, () => handleChangesAnimation());
+
+
+async function handleChangesAnimation () {
+  if (timeoutID) {
+    clearTimeout(timeoutID);
+    showAnimation.value = false;
+  }
+
+  setTimeout(() => showAnimation.value = true, 0);
+  timeoutID = setTimeout(() => showAnimation.value = false, 200);
+}
 </script>
 
 <template>
   <div class="live-price">
-    {{ getSymbolFromCurrency(props.unit) }} {{ props.price.bid.toFixed(5) }}
+    <span :class="showAnimation ? 'animation-fluctuation':''">
+      {{ getSymbolFromCurrency(props.unit) }} {{ props.price.bid.toFixed(5) }}
+    </span>
   </div>
 </template>
 
 
 <style lang="scss" scoped>
 @import "@/scss/local/_index.scss";
+
 .live-price {
   text-align: end;
   font-weight: bold;
@@ -30,5 +51,27 @@ const props = defineProps({
   @include breakpoint("lg") {
     font-size: 25px;
   }
+}
+
+@keyframes fluctuation {
+  0% {
+    color: $black;
+  }
+  25% {
+    color: rgba($black, .75);
+  }
+  50% {
+    color: rgba($black, .50);
+  }
+  75% {
+    color: rgba($black, .25);
+  }
+  100% {
+    color: rgba($black, 0);
+  }
+}
+
+.animation-fluctuation {
+  animation: fluctuation 200ms;
 }
 </style>
