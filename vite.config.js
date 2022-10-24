b71b57ac47c10f326ca9b375674c611b185fca4f
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-10-24 13:03:12 +0300

    Console logs has been deleted in production mode

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    strictPort: true,
-    port: 8080,
-    host: "::", //'0.0.0.0',
-  },
+export default defineConfig(({ command, mode }) => {
+
+    return {
+        plugins: [ vue() ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        server: {
+            strictPort: true,
+            port: 8080,
+            host: "::", //'0.0.0.0',
+        },
+        esbuild: {
+            drop: mode == "production" ? [ "console", "debugger" ] : []
+        }
+    };
+
 })
